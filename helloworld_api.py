5858764800aa767a29a312245100297e4dd6140e
Abhi Khanal <khanal.abhi@gmail.com>
2016-03-31 21:27:27 -0400

    all good.

--- helloworld_api.py
+++ helloworld_api.py
@@ -42,5 +42,10 @@ class HelloWorldApi(remote.Service):
       greet = "Hello {}".format(request.name)
       return Hello(greeting=greet)
 
+    @endpoints.method(REQUEST_CONTAINER, Hello, path="sayHelloByPeriod",
+                      http_method='GET', name="sayHelloByPeriod")
+    def say_hello_by_peroid(self, request):
+        greet = "Hello {}, Good {}".format(request.name, request.period)
+        return Hello(greeting=greet)
 
 APPLICATION = endpoints.api_server([HelloWorldApi])
